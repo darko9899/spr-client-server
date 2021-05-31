d11f0a33212bb25c92142b4b0b6affc3c7d63bc1
dpetrov360 <dpetrov360@gmail.com>
2021-06-01 01:05:24 +0300

    final

--- client.c
+++ client.c
@@ -4,74 +4,75 @@
 #include <string.h>
 #include <sys/socket.h>
 #define MAX 80
-#define PORT 8080
+#define PORT 8081
 #define SIZE 2000
 #define SA struct sockaddr
 
-typedef struct Response {
+typedef struct Response
+{
     int size;
     char arr[SIZE];
 } Response;
-void func(int sockfd)
+void chatFunc(int sockfd)
 {
-    int num=0;
+    int num = 0;
     char buff[SIZE];
-    for (;;) {
-        Response *response= (Response*)malloc(sizeof(Response));
-        printf("Enter step for iteration(0 for exit):");
+    for (;;)
+    {
+        Response *response = (Response *)malloc(sizeof(Response));
+        printf("Enter step for iteration:");
         scanf("%d", &num);
-        if(num<0){
+        if (num < 0)
+        {
             printf("Invalid step!!!\n");
             continue;
         }
         write(sockfd, &num, sizeof(int));
-        bzero(response,sizeof(Response));
-        bzero(buff,SIZE);
-        read(sockfd,buff,SIZE);
-        memcpy(response,buff,SIZE);
+        bzero(response, sizeof(Response));
+        bzero(buff, SIZE);
+        read(sockfd, buff, SIZE);
+        memcpy(response, buff, SIZE);
         printf("From Server : \n");
-        
+
         printf("Number of elements : %d\n", response->size);
-        printf("Result :\n%s\n",response->arr);
-        
-        if (num == 0) {
+        printf("Result :\n%s\n", response->arr);
+
+        if (num == 0)
+        {
             printf("Client Exit...\n");
             break;
         }
     }
 }
- 
+
 int main()
 {
     int sockfd, connfd;
     struct sockaddr_in servaddr, cli;
- 
-    // socket create and varification
+
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1) {
-        printf("socket creation failed...\n");
+    if (sockfd == -1)
+    {
+        printf("Error: Socket creation failed!!!\n");
         exit(0);
     }
     else
-        printf("Socket successfully created..\n");
+        printf("Socket successfully created...\n");
     bzero(&servaddr, sizeof(servaddr));
- 
-    // assign IP, PORT
+
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
     servaddr.sin_port = htons(PORT);
- 
-    // connect the client socket to server socket
-    if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) {
-        printf("connection with the server failed...\n");
+
+    if (connect(sockfd, (SA *)&servaddr, sizeof(servaddr)) != 0)
+    {
+        printf("Error: Connection with the server failed!!!\n");
         exit(0);
     }
     else
-        printf("connected to the server..\n");
- 
-    // function for chat
-    func(sockfd);
- 
-    // close the socket
+        printf("Connected to the server...\n");
+
+    chatFunc(sockfd);
+
     close(sockfd);
 }
--- server.c
+++ server.c
@@ -10,8 +10,9 @@
 #include <fcntl.h>
 #include <math.h>
 #define MAX 128
-#define PORT 8080
+#define PORT 8081
 #define SIZE 2000
+#define LENGTH 21
 #define FILENAME "test.bin"
 #define SA struct sockaddr
 
@@ -26,50 +27,42 @@ typedef struct Arr
 } Arr;
 struct list
 {
-    char string[50];
+    char data[MAX];
     struct list *next;
 };
 typedef struct list LIST;
 
 void makeResponse(int step, int sockfd)
 {
-    
-   
-    LIST *current, *head=NULL;
-    
+
+    LIST *current, *head = NULL;
+
     char buff[SIZE];
     int j = 0;
     char result[SIZE];
     Response *response = (Response *)malloc(sizeof(Response));
-    
-    int readSize=0;
 
-    
-
- 
     int fd = open(FILENAME, O_RDONLY);
-    if(fd==-1){
+    if (fd == -1)
+    {
         printf("Error: file test.bin cannot be opened\n");
         exit(1);
     }
- 
-   
-   Arr arrNew[21];
-   
-    int count=0;
-    while(read(fd,&arrNew[count],sizeof(Arr))==sizeof(Arr)){
-    count++;
-   
-    }
-    char test[50];
-  
-     
-     close(fd);
 
-     for(int i=0;i<sizeof(arrNew)/sizeof(Arr);i++)
+    Arr arrNew[LENGTH];
+
+    int count = 0;
+    while (read(fd, &arrNew[count], sizeof(Arr)) == sizeof(Arr))
+    {
+        count++;
+    }
+
+    close(fd);
+
+    for (int i = 0; i < sizeof(arrNew) / sizeof(Arr); i++)
     {
         LIST *node = malloc(sizeof(LIST));
-        strcpy(node->string,arrNew[i].array); //note : strdup is not standard function
+        strcpy(node->data, arrNew[i].array);
         node->next = NULL;
 
         if (head == NULL)
@@ -90,7 +83,7 @@ void makeResponse(int step, int sockfd)
     for (current = head; current; current = current->next)
     {
         strcat(response->arr, "\n");
-        strcat(response->arr, current->string);
+        strcat(response->arr, current->data);
         for (int i = 0; i < step - 1; i++)
         {
             if (current->next == NULL)
@@ -106,25 +99,24 @@ void makeResponse(int step, int sockfd)
     printf("To client :\n");
     printf("%s\n", response->arr);
     bzero(buff, SIZE);
-    response->size = size-1;
+    response->size = size - 1;
 
     memcpy(buff, response, sizeof(buff));
     write(sockfd, buff, sizeof(buff));
 }
 
-// Function designed for chat between client and server.
 void chatFunc(int sockfd)
 {
     int num = 0;
-    // infinite loop for chat
+
     for (;;)
     {
         printf("Waiting for client to send step for iteration...\n");
-        // read the message from client and copy it in buffer
+
         read(sockfd, &num, sizeof(int));
-        // print buffer which contains the client contents
+
         printf("From client: %d\n", num);
-       
+
         if (num == 0)
         {
             printf("Server Exit...\n");
@@ -134,13 +126,11 @@ void chatFunc(int sockfd)
     }
 }
 
-// Driver function
 int main()
 {
     int sockfd, connfd, len;
     struct sockaddr_in servaddr, cli;
 
-    // socket create and verification
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd == -1)
     {
@@ -151,43 +141,37 @@ int main()
         printf("Socket successfully created..\n");
     bzero(&servaddr, sizeof(servaddr));
 
-    // assign IP, PORT
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(PORT);
 
-    // Binding newly created socket to given IP and verification
     if ((bind(sockfd, (SA *)&servaddr, sizeof(servaddr))) != 0)
     {
-        printf("socket bind failed...\n");
+        printf("Error: Socket bind failed!!!\n");
         exit(0);
     }
     else
-        printf("Socket successfully binded..\n");
+        printf("Socket successfully binded...\n");
 
-    // Now server is ready to listen and verification
     if ((listen(sockfd, 5)) != 0)
     {
-        printf("Listen failed...\n");
+        printf("Error: Listen failed!!!\n");
         exit(0);
     }
     else
-        printf("Server listening..\n");
+        printf("Server listening...\n");
     len = sizeof(cli);
 
-    // Accept the data packet from client and verification
     connfd = accept(sockfd, (SA *)&cli, &len);
     if (connfd < 0)
     {
-        printf("server acccept failed...\n");
+        printf("Error: Server acccept failed!!!\n");
         exit(0);
     }
     else
-        printf("server acccept the client...\n");
+        printf("Server acccept the client...\n");
 
-    // Function for chatting between client and server
     chatFunc(connfd);
 
-    // After chatting close the socket
     close(sockfd);
 }
